8ff5738b254528109aa83e3ce18bdab03a8c4f94
AsifR16 <asifr8054@gmail.com>
2026-08-08 18:47:12 +0530

    Added schema for entities

--- database/schema.sql
+++ database/schema.sql
@@ -3,3 +3,9 @@ CREATE TABLE IF NOT EXISTS "banks" (
         "bank_name"     VARCHAR(128) NOT NULL,
         PRIMARY KEY("bank_id")
 );
+
+CREATE TABLE IF NOT EXISTS "entities" (
+        "entity_id"     INTEGER,
+        "entity_name"   VARCHAR(128) NOT NULL,
+        PRIMARY KEY("entity_id")
+);
